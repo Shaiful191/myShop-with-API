253b369331ca45934ac587dda792abce0e454bd2
Shaiful Islam <118191679+Shaiful191@users.noreply.github.com>
2024-01-21 21:59:14 +0600

    getbuilder added

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -24,15 +24,14 @@ class _HomePageState extends State<HomePage> {
   void initState() {
     // fetchCarouselImages();
     // fetchProducts();
-   productController.productList;
-   productController.categoriesList;
+    productController.productList;
+    productController.categoriesList;
     super.initState();
   }
 
- 
   var categoriesImageList = [12, 7, 2, 14];
 
-  ProductController productController =  Get.put(ProductController());
+  ProductController productController = Get.put(ProductController());
 //  late ProductController productController;
 //   _loadData() async {
 //    productController = await Get.put(ProductController());
@@ -41,88 +40,86 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-   //_loadData();
+    //_loadData();
     var height = MediaQuery.of(context).size.height;
     var width = MediaQuery.of(context).size.width;
 
     return Scaffold(
-    
+      body: GetBuilder<ProductController>(builder: (product) {
+        return Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              SizedBox(
+                height: height * 0.06,
+              ),
+              HomePageAppBar(),
+              HomePageOfferCard(height: height),
+              SizedBox(
+                height: height * 0.01,
+              ),
+              SubHeadline(
+                leadingName: "Categories",
+                trailingName: "See All",
+              ),
+              Container(
+                height: height * 0.15,
+                child: Obx(() {
+                  if (productController.isLoading.value) {
+                    return Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  } else {
+                    //  return Text( productController.productList[0]["title"].toString());
+                    // print(productController.categoriesList[0]);
+                    return Expanded(
+                      // flex: 1,
+                      child: ListView.separated(
+                        scrollDirection: Axis.horizontal,
+                        itemCount: productController.categoriesList.length,
+                        //  itemCount: 4,
+                        itemBuilder: (BuildContext context, int index) {
+                          return CategoriesCard(
+                            productController: productController,
+                            width: width,
+                            height: height * 0.15,
+                            //  color: colorList[index],
+                            //   color: colorList[index],
+                            index: index,
+                            ImageListindex: categoriesImageList[index],
+                            //  categoriesName: "Bread",
+                          );
+                        },
+                        separatorBuilder: (BuildContext context, int index) {
+                          return SizedBox(
+                            width: width * 0.04,
+                          );
+                        },
+                      ),
+                    );
+                  }
+                }),
+              ),
+              SizedBox(
+                height: height * 0.02,
+              ),
+              SubHeadline(
+                leadingName: "Best Selling",
+                trailingName: "See All",
+              ),
+              //  Obx(() {
+              //       return Text(
+              //         controller.counter.toString(),
+              //         style: Theme.of(context).textTheme.headlineMedium,
+              //       );
+              //     }),
 
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            SizedBox(
-              height: height * 0.06,
-            ),
-            HomePageAppBar(),
-            HomePageOfferCard(height: height),
-            SizedBox(
-              height: height * 0.01,
-            ),
-            SubHeadline(
-              leadingName: "Categories",
-              trailingName: "See All",
-            ),
-            Container(
-              height: height * 0.15,
-              child: Obx(() {
-                if (productController.isLoading.value) {
-                  return Center(
-                    child: CircularProgressIndicator(),
-                  );
-                } else {
-                  //  return Text( productController.productList[0]["title"].toString());
-                  // print(productController.categoriesList[0]);
-                  return Expanded(
-                    // flex: 1,
-                    child: ListView.separated(
-                      scrollDirection: Axis.horizontal,
-                      itemCount:productController.categoriesList.length,
-                    //  itemCount: 4,
-                      itemBuilder: (BuildContext context, int index) {
-                        return CategoriesCard(
-                          productController: productController,
-                          width: width,
-                          height: height * 0.15,
-                          //  color: colorList[index],
-                          //   color: colorList[index],
-                          index: index,
-                          ImageListindex: categoriesImageList[index],
-                          //  categoriesName: "Bread",
-                        );
-                      },
-                      separatorBuilder: (BuildContext context, int index) {
-                        return SizedBox(
-                          width: width * 0.04,
-                        );
-                      },
-                    ),
-                  );
-                }
-              }),
-            ),
-            SizedBox(
-              height: height * 0.02,
-            ),
-            SubHeadline(
-              leadingName: "Best Selling",
-              trailingName: "See All",
-            ),
-            //  Obx(() {
-            //       return Text(
-            //         controller.counter.toString(),
-            //         style: Theme.of(context).textTheme.headlineMedium,
-            //       );
-            //     }),
-
-            Expanded(
-              child: 
-                ListView.separated(
+              Expanded(
+                child: ListView.separated(
                   padding: EdgeInsets.all(0),
-                //  itemCount: productController.categoriesList.length,
-                itemCount: 7,
+                  //  itemCount: productController.categoriesList.length,
+                  itemCount: 7,
                   separatorBuilder: (BuildContext context, int index) {
                     return SizedBox(
                       height: height * 0.02,
@@ -137,13 +134,11 @@ class _HomePageState extends State<HomePage> {
                     );
                   },
                 ),
-              
-              
-            ),
-          ],
-        ),
-      ),
-
+              ),
+            ],
+          ),
+        );
+      }),
       bottomNavigationBar: CustomBottomNavigationBar(),
     );
   }
@@ -201,112 +196,109 @@ class BestSellingCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return
-    Obx(() {
-      return  Container(
-      decoration: BoxDecoration(
-        color: Color(0xffF0F0F0),
-        borderRadius: BorderRadius.circular(8),
-      ),
-      width: double.infinity,
-      height: height * 0.16,
-      child: Padding(
-        padding: EdgeInsets.only(
-          left: width * 0.04,
-          top: height * 0.02,
-          bottom: height * 0.02,
+    return Obx(() {
+      return Container(
+        decoration: BoxDecoration(
+          color: Color(0xffF0F0F0),
+          borderRadius: BorderRadius.circular(8),
         ),
-        child: 
-        
-        Row(
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                color: Color(0xffCEE7D4),
-                borderRadius: BorderRadius.circular(5),
-              ),
-              width: width * 0.26,
-              height: height * 0.12,
-              child: Image.network(
-                productController.productList[BestSellingindex]["image"]
-                    .toString(),
-                fit: BoxFit.cover,
-              ),
+        width: double.infinity,
+        height: height * 0.16,
+        child: Padding(
+          padding: EdgeInsets.only(
+            left: width * 0.04,
+            top: height * 0.02,
+            bottom: height * 0.02,
+          ),
+          child: Row(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  color: Color(0xffCEE7D4),
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                width: width * 0.26,
+                height: height * 0.12,
+                child: Image.network(
+                  productController.productList[BestSellingindex]["image"]
+                      .toString(),
+                  fit: BoxFit.cover,
+                ),
 
-              //  Icon(
-              //   Icons.food_bank,
-              //   size: 40,
-              //   color: AppColors.primary,
-              // ),
-            ),
-            SizedBox(
-              width: width * 0.06,
-            ),
-            Container(
-              width: width * 0.36,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    productController.productList[BestSellingindex]["title"]
-                        .toString(),
-                    //     "Orange Fresh Juice",
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.w500,
-                      color: Colors.black,
-                    ),
-                  ),
-                  Text(
-                    productController.productList[BestSellingindex]
-                            ["description"]
-                        .toString(),
-                    // "It dummy reasonable to laying Lorem words to.",
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
-                    style: TextStyle(
-                      color: Colors.black,
-                    ),
-                  ),
-                  Text(
-                    "TK." +
-                        productController.productList[BestSellingindex]["price"]
-                            .toString(),
-                    // "Tk.550",
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black,
-                    ),
-                  ),
-                ],
+                //  Icon(
+                //   Icons.food_bank,
+                //   size: 40,
+                //   color: AppColors.primary,
+                // ),
               ),
-            ),
-            SizedBox(
-              width: width * 0.002,
-            ),
-            Align(
-              alignment: Alignment.topLeft,
-              child: IconButton(
-                padding: EdgeInsets.only(
-                  left: width * 0.02,
-                ),
-                onPressed: () {},
-                icon: Icon(
-                  Icons.favorite_outline,
-                  color: AppColors.primary,
+              SizedBox(
+                width: width * 0.06,
+              ),
+              Container(
+                width: width * 0.36,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      productController.productList[BestSellingindex]["title"]
+                          .toString(),
+                      //     "Orange Fresh Juice",
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.black,
+                      ),
+                    ),
+                    Text(
+                      productController.productList[BestSellingindex]
+                              ["description"]
+                          .toString(),
+                      // "It dummy reasonable to laying Lorem words to.",
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(
+                        color: Colors.black,
+                      ),
+                    ),
+                    Text(
+                      "TK." +
+                          productController.productList[BestSellingindex]
+                                  ["price"]
+                              .toString(),
+                      // "Tk.550",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ],
                 ),
               ),
-            )
-          ],
+              SizedBox(
+                width: width * 0.002,
+              ),
+              Align(
+                alignment: Alignment.topLeft,
+                child: IconButton(
+                  padding: EdgeInsets.only(
+                    left: width * 0.02,
+                  ),
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.favorite_outline,
+                    color: AppColors.primary,
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
-      ),
-    );
+      );
     });
-    
   }
 }
 
